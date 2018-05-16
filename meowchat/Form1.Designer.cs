0c29bc8f3b3abfbc6ee7d9baab103014186b3fec
Mikhailiuk Kseniya <mikhailiuk.k@gmail.com>
2018-05-16 20:54:50 +0300

    UDP video chat

---
+++ meowchat/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace meowchat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxMine = new System.Windows.Forms.PictureBox();
+            this.camToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.labelPortListen = new System.Windows.Forms.Label();
+            this.labelMyPort = new System.Windows.Forms.Label();
+            this.labelServerIP = new System.Windows.Forms.Label();
+            this.textBoxFriendPort = new System.Windows.Forms.TextBox();
+            this.textBoxMyPort = new System.Windows.Forms.TextBox();
+            this.textBoxServerIP = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.pictureBoxFriend = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMine)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFriend)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxMine
+            // 
+            this.pictureBoxMine.BackColor = System.Drawing.Color.White;
+            this.pictureBoxMine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxMine.Location = new System.Drawing.Point(12, 423);
+            this.pictureBoxMine.Name = "pictureBoxMine";
+            this.pictureBoxMine.Size = new System.Drawing.Size(774, 500);
+            this.pictureBoxMine.TabIndex = 0;
+            this.pictureBoxMine.TabStop = false;
+            // 
+            // camToolStripMenuItem
+            // 
+            this.camToolStripMenuItem.Name = "camToolStripMenuItem";
+            this.camToolStripMenuItem.Size = new System.Drawing.Size(123, 38);
+            this.camToolStripMenuItem.Text = "WebCam";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(75, 36);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(75, 36);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.camToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2439, 42);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // labelPortListen
+            // 
+            this.labelPortListen.AutoSize = true;
+            this.labelPortListen.Location = new System.Drawing.Point(45, 94);
+            this.labelPortListen.Name = "labelPortListen";
+            this.labelPortListen.Size = new System.Drawing.Size(108, 25);
+            this.labelPortListen.TabIndex = 4;
+            this.labelPortListen.Text = "Port listen";
+            // 
+            // labelMyPort
+            // 
+            this.labelMyPort.AutoSize = true;
+            this.labelMyPort.Location = new System.Drawing.Point(45, 164);
+            this.labelMyPort.Name = "labelMyPort";
+            this.labelMyPort.Size = new System.Drawing.Size(86, 25);
+            this.labelMyPort.TabIndex = 5;
+            this.labelMyPort.Text = "My Port";
+            // 
+            // labelServerIP
+            // 
+            this.labelServerIP.AutoSize = true;
+            this.labelServerIP.Location = new System.Drawing.Point(45, 235);
+            this.labelServerIP.Name = "labelServerIP";
+            this.labelServerIP.Size = new System.Drawing.Size(100, 25);
+            this.labelServerIP.TabIndex = 6;
+            this.labelServerIP.Text = "Server IP";
+            // 
+            // textBoxFriendPort
+            // 
+            this.textBoxFriendPort.Location = new System.Drawing.Point(50, 122);
+            this.textBoxFriendPort.Name = "textBoxFriendPort";
+            this.textBoxFriendPort.Size = new System.Drawing.Size(145, 31);
+            this.textBoxFriendPort.TabIndex = 7;
+            this.textBoxFriendPort.Text = "8008";
+            // 
+            // textBoxMyPort
+            // 
+            this.textBoxMyPort.Location = new System.Drawing.Point(50, 192);
+            this.textBoxMyPort.Name = "textBoxMyPort";
+            this.textBoxMyPort.Size = new System.Drawing.Size(145, 31);
+            this.textBoxMyPort.TabIndex = 8;
+            this.textBoxMyPort.Text = "8009";
+            // 
+            // textBoxServerIP
+            // 
+            this.textBoxServerIP.Location = new System.Drawing.Point(50, 263);
+            this.textBoxServerIP.Name = "textBoxServerIP";
+            this.textBoxServerIP.Size = new System.Drawing.Size(145, 31);
+            this.textBoxServerIP.TabIndex = 9;
+            this.textBoxServerIP.Text = "127.0.0.1";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(299, 192);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(143, 47);
+            this.buttonConnect.TabIndex = 10;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // pictureBoxFriend
+            // 
+            this.pictureBoxFriend.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBoxFriend.Location = new System.Drawing.Point(809, 43);
+            this.pictureBoxFriend.Name = "pictureBoxFriend";
+            this.pictureBoxFriend.Size = new System.Drawing.Size(1630, 1135);
+            this.pictureBoxFriend.TabIndex = 11;
+            this.pictureBoxFriend.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2439, 1190);
+            this.Controls.Add(this.pictureBoxFriend);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxServerIP);
+            this.Controls.Add(this.textBoxMyPort);
+            this.Controls.Add(this.textBoxFriendPort);
+            this.Controls.Add(this.labelServerIP);
+            this.Controls.Add(this.labelMyPort);
+            this.Controls.Add(this.labelPortListen);
+            this.Controls.Add(this.pictureBoxMine);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MEOWCHAT";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMine)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFriend)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxMine;
+        private System.Windows.Forms.ToolStripMenuItem camToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label labelPortListen;
+        private System.Windows.Forms.Label labelMyPort;
+        private System.Windows.Forms.Label labelServerIP;
+        private System.Windows.Forms.TextBox textBoxFriendPort;
+        private System.Windows.Forms.TextBox textBoxMyPort;
+        private System.Windows.Forms.TextBox textBoxServerIP;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.PictureBox pictureBoxFriend;
+    }
+}
+
---
+++ meowchat/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace meowchat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            PictureBox pictureBox = new PictureBox();
+            startToolStripMenuItem.Enabled = false;
+            stopToolStripMenuItem.Enabled = false;
+        }
+        
+        static string remoteAddress; // хост для отправки данных
+        static int FriendPort; // порт для отправки данных
+        static int MyPort; // локальный порт для прослушивания входящих подключений
+
+        private VideoCaptureDevice WebCam;
+        private FilterInfoCollection CamCollection;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CamCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            ToolStripMenuItem menuItem = new ToolStripMenuItem();
+            foreach (FilterInfo cam in CamCollection)
+            {
+                menuItem = new ToolStripMenuItem()
+                {
+                    Text = cam.Name,
+                    Tag = cam.MonikerString
+                };
+                menuItem.Click += new EventHandler(MenuItemCamClickHandler);
+                camToolStripMenuItem.DropDownItems.Add(menuItem);
+            }
+
+            camToolStripMenuItem.Enabled = false;
+        }
+        //Thread sendThread = new Thread(new ParameterizedThreadStart(SendMessage));
+        private void SendMessage(/*object form*/)
+        {
+            UdpClient sender = new UdpClient(); // создаем UdpClient для отправки сообщений
+            try
+            {
+                //Form1 form1 = (Form1)form;
+                while (stopToolStripMenuItem.Pressed)// && form1.pictureBoxMine.Image != null)
+                {
+                    if (pictureBoxMine.Image != null)
+                    { 
+                        string data = JsonConvert.SerializeObject(new Bitmap(pictureBoxMine.Image), new JsonSerializerSettings()
+                        {
+                            TypeNameHandling = TypeNameHandling.All
+                        });
+                        string readyString = Convert.ToBase64String(Encoding.Default.GetBytes(data));
+                        try
+                        {
+                            sender.Send(Encoding.Default.GetBytes(readyString), readyString.Length, remoteAddress, FriendPort); // отправка
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBoxButtons button = MessageBoxButtons.OK;
+                            string caption = "Error";
+                            MessageBox.Show(ex.Message, caption, button);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+            }
+        }
+
+        private static void ReceiveMessage(object form)
+        {
+            UdpClient receiver = new UdpClient(MyPort); // UdpClient для получения данных
+            IPAddress ip = IPAddress.Parse(remoteAddress);
+            byte[] ipbytes = ip.GetAddressBytes();
+
+            IPEndPoint remoteIp = new IPEndPoint(ip, FriendPort); // адрес входящего подключения
+            try
+            {
+                Form1 form1 = (Form1)form;
+                while (!form1.stopToolStripMenuItem.Pressed)
+                {
+                    try
+                    {
+                        byte[] data = receiver.Receive(ref remoteIp);
+                        string message = Encoding.ASCII.GetString(data);
+                        byte[] result = Convert.FromBase64String(message);
+                        Image image = JsonConvert.DeserializeObject<Bitmap>(Encoding.Default.GetString(result),
+                            new JsonSerializerSettings
+                            {
+                                TypeNameHandling = TypeNameHandling.All
+                            });
+
+                        form1.pictureBoxFriend.Image = image;
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBoxButtons button = MessageBoxButtons.OK;
+                        string caption = "Error";
+                        MessageBox.Show(ex.Message, caption, button);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                receiver.Close();
+            }
+        }
+
+        private void MenuItemCamClickHandler(object sender, EventArgs e)
+        {
+            camToolStripMenuItem.Enabled = false;
+            ToolStripMenuItem clickedItem = (ToolStripMenuItem)sender;
+            try
+            {
+                WebCam = new VideoCaptureDevice(clickedItem.Tag.ToString());
+                if (!WebCam.IsRunning)
+                {
+                    WebCam.NewFrame += WebCam_NewFrame;
+                    WebCam.Start();
+                    startToolStripMenuItem.Enabled = true;
+                    camToolStripMenuItem.Enabled = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                string caption = "Error";
+                MessageBox.Show(ex.Message, caption, button);
+            }
+
+        }
+
+        private void WebCam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = new Bitmap((Bitmap)eventArgs.Frame.Clone(), pictureBoxMine.Size);
+            pictureBoxMine.Image = bitmap/*(Image)eventArgs.Frame.Clone()*/;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //WebCam.NewFrame += WebCam_NewFrame;
+            //WebCam.Start();
+            stopToolStripMenuItem.Enabled = true;
+            startToolStripMenuItem.Enabled = false;
+            //sendThread.Start(this);
+            SendMessage(/*this*/);
+        }
+
+        private void Abort()
+        {
+            if (WebCam!= null)
+            {
+                WebCam.Stop();
+            }
+            if (receiveThread.IsAlive)
+            {
+                receiveThread.Abort();
+            }
+            //if (sendThread.IsAlive)
+            //{
+            //    sendThread.Abort();
+            //}
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (WebCam.IsRunning)
+            {
+                WebCam.Stop();
+            }
+            Graphics g = pictureBoxMine.CreateGraphics();
+            g.Clear(Color.White);
+            startToolStripMenuItem.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Abort();
+        }
+
+        Thread receiveThread = new Thread(new ParameterizedThreadStart(ReceiveMessage));
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if ((new Regex(@"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b")).IsMatch(textBoxServerIP.Text) &&
+                    (new Regex(@"\b[0-9]+\b")).IsMatch(textBoxMyPort.Text) &&
+                    (new Regex(@"\b[0-9]+\b")).IsMatch(textBoxFriendPort.Text))
+            {
+                try
+                {
+                    MyPort = Int32.Parse(textBoxMyPort.Text);
+                    remoteAddress = textBoxServerIP.Text; // адрес, к которому мы подключаемся
+                    FriendPort = Int32.Parse(textBoxFriendPort.Text); // порт, к которому мы подключаемся
+                    string infoMessage = "Аминь";
+                    string caption = "bless and save";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    MessageBox.Show(infoMessage, caption, buttons);
+                    camToolStripMenuItem.Enabled = true;
+                    receiveThread.Start(this);
+                    buttonConnect.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxButtons button = MessageBoxButtons.OK;
+                    string caption = "Error";
+                    MessageBox.Show(ex.Message, caption, button);
+                }
+            }
+        }
+    }
+}
